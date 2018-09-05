0d56d71732eb89dc7b5af62896073e4b1d23f1ed
Domenico Popolizio <domenico9501@outlook.it>
2018-09-05 23:17:33 +0200

    Added .gitignore for firebase file Keyboard keydown event replaced with keyup (it look more responsive) If you play with touch events you cannot use keyobard and viceversa Mode (touch or keyboard) choosen based on first arrow destroy mode Can start with Enter key Movement checking changed, it check if dir is right, not chosen or wrong Nowtouch event work on with the necessary coordinate (X or Y)

--- public/js/index.js
+++ public/js/index.js
@@ -36,6 +36,7 @@ let recordElem;
 let lastElem;
 let menuElem;
 let started = false;
+let playing = false;
 
 let toTouch = 1/5; //arrow sesction to touch to eliminate
 
@@ -48,7 +49,7 @@ try {
     record = 0;
 } 
 
-
+let mode = undefined;
 
 //funzioni
 const getTop = r => {
@@ -61,22 +62,13 @@ const getTop = r => {
 } 
 
 const randrange = ( min, max ) => Math.floor(Math.random() * (max - min)) + min; //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
-const checkTouch = (dir, dx, dy ) => {
 
-    switch(dir) {
-        case 'up':
-            return dy<0 && dy<(MaxS*toTouch);
-            break;
-        case 'down':
-            return dy>0 && dy>(MaxS*toTouch);
-            break;
-        case 'right':
-            return dx>0 && dx>(MaxS*toTouch);
-            break;
-        case 'left':
-            return dx<0 && dx<(MaxS*toTouch);
-            break;
-    }
+
+const getDir = (delta) => {
+    if(delta < -(MaxS*toTouch)) return -1;
+    if(delta > +(MaxS*toTouch)) return 1;
+    return 0;
+
 }
 
 const writeScore = () => scoreElem.innerHTML = score 
@@ -99,23 +91,22 @@ function addPoint() {
 }
 
 function targetingArrow(event) {
+    if(mode == undefined) mode = "touch";
+    if(mode == "keyboard") return;
     let touches = event.changedTouches[0];
-    event.target.dataset.xi = touches.pageX/*-parseFloat(event.target.style.left)*/;
-    event.target.dataset.yi = touches.pageY/*-parseFloat(event.target.style.top)*/;
+    event.target.dataset.ci = touches["page"+event.target.dataset.coord];
 }
 
 function movingOnArrow(event) {
     let touches = event.changedTouches[0];
-    let xf = touches.pageX/*-parseFloat(event.target.style.left)*/;
-    let yf = touches.pageY/*-parseFloat(event.target.style.top)*/;
-    let dx = xf-event.target.dataset.xi;
-    let dy = yf-event.target.dataset.yi;
-    console.log(dx, dy);
-    //console.log(xi, xf, dx, yi, yf, dy);
-    if(checkTouch(event.target.dataset.dir, dx, dy)) {
+    let cf = touches["page"+event.target.dataset.coord];
+    let dc = cf-event.target.dataset.ci;
+    
+    if(getDir(dc) == event.target.dataset.dir) {
         
         addPoint();
         removeArrow(event.target);
+
         event.target.removeEventListener('touchstart', targetingArrow);
         event.target.removeEventListener('touchmove', movingOnArrow);
     }
@@ -128,7 +119,14 @@ function addArrow() {
     let dir = directions[randrange(0, directions.length-1)];
     newArrow.classList.add(dir);
 
-    newArrow.dataset.dir = dir;
+    if(dir == "up" || dir == "down") {
+        newArrow.dataset.coord = "Y";
+        newArrow.dataset.dir = dir=="up" ? -1 : 1;
+    }
+    else {
+        newArrow.dataset.coord = "X";
+        newArrow.dataset.dir = dir=="left" ? -1 : 1;
+    }
 
     document.body.appendChild(newArrow);
 
@@ -139,9 +137,10 @@ function addArrow() {
     
     
    
-
-    newArrow.addEventListener('touchstart', targetingArrow );
-    newArrow.addEventListener('touchmove', movingOnArrow );
+    if(mode == "touch"|| mode == undefined) {    
+        newArrow.addEventListener('touchstart', targetingArrow );
+        newArrow.addEventListener('touchmove', movingOnArrow );
+    }
 
     
     arrows = document.querySelectorAll('.arrow');
@@ -166,7 +165,6 @@ function removeArrow(r) {
 }
 
 function frame() {
-
     //let px = vr * (tick-lastTick);
     //console.log(px);
     if ( getTop(arrows[arrows.length-1]) >= 15/*px >= Rh+75*/ ) {
@@ -203,17 +201,37 @@ function frame() {
 }
 
 function onKeyPress(event) {
+
+    
     let dir = keysToArrows[event.key];
-    let rs4keysEv = document.querySelectorAll('.arrow:not(.destroy)'); //arrows for keys event
-    let targetArrow = rs4keysEv[0];
-    if( targetArrow.classList.contains(dir) ) {
-        removeArrow(targetArrow);
-        addPoint();
+
+          
+    if(playing) {
+
+        if(dir != undefined) {  
+            if(mode == undefined) mode = "keyboard";
+            if(mode == "touch") return;
+
+            let rs4keysEv = document.querySelectorAll('.arrow:not(.destroy)'); //arrows for keys event
+            if(rs4keysEv.length > 0) {
+                let targetArrow = rs4keysEv[0];
+                if( targetArrow.classList.contains(dir) ) {
+                    removeArrow(targetArrow);
+                    addPoint();
+                }
+            }
+        }
     }
+    else {
+        if(event.key == "Enter") start();
+    }
+    
 }
 
 function start() {
+    mode = undefined;
     started = true;
+    playing = true;
     closeMenu();
     score = 0;
     vr = vri;
@@ -221,15 +239,12 @@ function start() {
     writeRecord();
     addArrow();
 
-    try{
-        document.removeEventListener('keyup',onKeyPress); 
-    } catch(e) {};
-    document.addEventListener('keyup',onKeyPress); 
 
     window.requestAnimationFrame(frame);
 }
 
 function openMenu() {
+    playing = false;
     writeRecord();
     writeLast();
 
@@ -248,6 +263,7 @@ window.addEventListener(
     () => {
 
         started = false;
+        playing = false;
 
         scoreElem = document.querySelector("#score");
         recordElem = document.querySelector("#menu-score #record");
@@ -255,6 +271,7 @@ window.addEventListener(
         menuElem = document.querySelector('#menu');
         
         document.querySelector("#start").addEventListener( 'click', start );
+        document.addEventListener('keydown',onKeyPress); 
 
         openMenu();
         
